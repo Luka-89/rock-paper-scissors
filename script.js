4361f5b0baa24ad8c4a4028ff60cf1b8695733e0
Luka Batricevic <lukabatricevic8989@gmail.com>
2025-07-20 18:53:45 +0200

    Add html start elements and style them

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("Hello World");
+let user = prompt("Rock paper scissors!");
+const choices = {rock: 0, paper: 1, scissors: 2};
+let userChoice = choices[user];
+let aiChoice = Math.floor(Math.random() * 2);
+console.log(user);
